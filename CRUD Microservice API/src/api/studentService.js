68ee6207d32188bbd6eea8b746385b8049f327a8
Karan Jadhav <142383819+KaranJadhav0027@users.noreply.github.com>
2025-08-11 08:31:01 +0530

    Delete CRUD Microservice API/src directory

--- CRUD Microservice API/src/api/studentService.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const BASE_URL = 'http://localhost:8080/batches';
-
-export const getAllBatches = () => axios.get(BASE_URL);
-export const getBatchById = (id) => axios.get(`${BASE_URL}/id/${id}`);
-export const createBatch = (data) => axios.post(BASE_URL, data);
-export const updateBatch = (id, data) => axios.put(`${BASE_URL}/id/${id}`, data);
-export const deleteBatch = (id) => axios.delete(`${BASE_URL}/id/${id}`);
